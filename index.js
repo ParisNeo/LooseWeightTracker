91273233a1f2f343efb8c7ab940a34b685fdb057
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2022-12-21 12:39:36 +0000

    Upgraded

--- index.js
+++ index.js
@@ -1,49 +1,80 @@
-const form = document.getElementById("form");
-const planSelect = document.getElementById("plan");
-const planInfoDiv = document.getElementById("plan-info");
+// Load the ShapeTracker object from local storage
+const ShapeTracker = JSON.parse(localStorage.getItem("ShapeTracker"));
 
-// Load the plans from the plans.json file and add them to the select element
-fetch("plans.json")
-  .then(response => response.json())
-  .then(plans => {
-    plans.forEach(plan => {
-      const option = document.createElement("option");
-      option.value = plan.plan_name;
-      option.text = plan.plan_name;
-      planSelect.add(option);
-    });
-  });
-
-form.addEventListener("submit", e => {
-  e.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const age = document.getElementById("age").value;
-  const weight = document.getElementById("weight").value;
-  const plan = document.getElementById("plan").value;
-
-  // Store the user's information in local storage
-  localStorage.setItem("name", name);
-  localStorage.setItem("age", age);
-  localStorage.setItem("weight", weight);
-  localStorage.setItem("plan", plan);
-
-  // Redirect to the main page
-  window.location.href = "main.html";
-});
-
-// Update the plan info div when the plan select element changes
-planSelect.addEventListener("change", e => {
+if (ShapeTracker) {
+  // ShapeTracker object is found, redirect to index page
+  window.location.href = "index.html";
+} else {
+  const form = document.getElementById("form");
+  const planSelect = document.getElementById("plan");
+  const planInfoDiv = document.getElementById("plan-info");
+  
+  // Load the plans from the plans.json file and add them to the select element
   fetch("plans.json")
     .then(response => response.json())
     .then(plans => {
-      const selectedPlan = plans.find(plan => plan.plan_name === e.target.value);
-      planInfoDiv.innerHTML = selectedPlan.plan_description;
-    })
-});
-// If the ShapeTracker object already exists in local storage, redirect to the main page on page load
-window.addEventListener("load", () => {
-    if (ShapeTracker.plan) {
-      window.location.href = "main.html";
-    }
+      plans.forEach(plan => {
+        const option = document.createElement("option");
+        option.value = plan.plan_name;
+        option.text = plan.plan_name;
+        planSelect.add(option);
+      });
+    });
+  
+  
+  // Start tracking function
+  function startTracking() {
+    // Get the form values
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    const weight = document.getElementById("weight").value;
+    const plan = document.getElementById("plan").value;
+  
+    // Create the ShapeTracker object
+    const ShapeTracker = {
+      name: name,
+      age: age,
+      weight: weight,
+      plan: plan,
+      weights: []
+    };
+  
+    // Save the ShapeTracker object to local storage
+    localStorage.setItem("ShapeTracker", JSON.stringify(ShapeTracker));
+  
+    // Redirect to the main page
+    window.location.href = "main.html";
+  }
+  
+  form.addEventListener("submit", e => {
+    e.preventDefault();
+  
+    startTracking();
   });
+  
+  
+  // Update the plan info div when the plan select element changes
+  planSelect.addEventListener("change", e => {
+    fetch("plans.json")
+      .then(response => response.json())
+      .then(plans => {
+        const selectedPlan = plans.find(plan => plan.plan_name === e.target.value);
+        planInfoDiv.innerHTML = selectedPlan.plan_description;
+      })
+  });
+  // If the ShapeTracker object already exists in local storage, redirect to the main page on page load
+  window.addEventListener("load", () => {
+      fetch("plans.json")
+      .then(response => response.json())
+      .then(plans => {
+        const selectedPlan = plans.find(plan => plan.plan_name === e.target.value);
+        planInfoDiv.innerHTML = selectedPlan.plan_description;
+      })
+  
+      if (ShapeTracker.plan) {
+        window.location.href = "main.html";
+      }
+    });
+
+}
+
