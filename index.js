8ea054910ff1d851530ab0a6539edeb18d269c5b
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2022-12-21 10:44:07 +0000

    added dynamic plans

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const form = document.getElementById("form");
+const planSelect = document.getElementById("plan");
+const planInfoDiv = document.getElementById("plan-info");
+
+// Load the plans from the plans.json file and add them to the select element
+fetch("plans.json")
+  .then(response => response.json())
+  .then(plans => {
+    plans.forEach(plan => {
+      const option = document.createElement("option");
+      option.value = plan.plan_name;
+      option.text = plan.plan_name;
+      planSelect.add(option);
+    });
+  });
 
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -16,4 +30,14 @@ form.addEventListener("submit", e => {
 
   // Redirect to the main page
   window.location.href = "main.html";
+});
+
+// Update the plan info div when the plan select element changes
+planSelect.addEventListener("change", e => {
+  fetch("plans.json")
+    .then(response => response.json())
+    .then(plans => {
+      const selectedPlan = plans.find(plan => plan.plan_name === e.target.value);
+      planInfoDiv.innerHTML = selectedPlan.plan_description;
+    })
 });
